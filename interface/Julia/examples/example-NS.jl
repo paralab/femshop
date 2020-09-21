7aff90dbf79d727f90ed92bd2db90c1bdd7d8f25
songzhex <songzhex@illinois.com>
2020-09-21 17:14:41 -0600

    making an NS example

---
+++ interface/Julia/examples/example-NS.jl
@@ -0,0 +1,92 @@
+#=
+# 2D NS eq. Dirichlet bc, CG
+=#
+if !@isdefined(Femshop)
+    include("../Femshop.jl");
+    using .Femshop
+end
+init_femshop("NS");
+
+# Try making an optional log
+@useLog("NSlog")
+
+# Set up the configuration (order doesn't matter)
+@domain(2, SQUARE, UNSTRUCTURED)    # dimension, geometry, decomposition
+@solver(CG)                         # DG, CG, etc.
+@functionSpace(LEGENDRE, 2)         # function, order (or use testFunction and trialFunction)
+@nodes(LOBATTO)                     # elemental node arrangement
+@stepper(EULER_IMPLICIT)            # time stepper (optional second arg is CFL#)
+
+# Specify the problem
+num_elem = 32;
+@mesh(QUADMESH, num_elem, 4)                   # .msh file or generate our own
+
+@variable(u)                        # same as @variable(u, SCALAR)
+@variable(v)                        # same as @variable(u, SCALAR)
+@variable(p)                        # same as @variable(u, SCALAR)
+@variable(du)                        # same as @variable(u, SCALAR)
+@variable(dv)                        # same as @variable(u, SCALAR)
+@variable(dp)                        # same as @variable(u, SCALAR)
+
+@testSymbol(v)                    # sets the symbol for a test function
+
+T = 1
+@timeInterval(T)                    # (start, end) using this sets problem to time dependent
+@initial(u, "0")  # initial condition needed if time dependent
+@initial(du, "0")  # initial condition needed if time dependent
+@initial(v, "0")  # initial condition needed if time dependent
+@initial(dv, "0")  # initial condition needed if time dependent
+@initial(p, "0")  # initial condition needed if time dependent
+@initial(dp, "0")  # initial condition needed if time dependent
+
+@boundary(u, 1, DIRICHLET, 0)
+@boundary(du, 1, DIRICHLET, 0)
+@boundary(v, 1, DIRICHLET, 0)
+@boundary(dv, 1, DIRICHLET, 0)
+@boundary(u, 2, DIRICHLET, 0)
+@boundary(du, 2, DIRICHLET, 0)
+@boundary(v, 2, DIRICHLET, 0)
+@boundary(dv, 2, DIRICHLET, 0)
+@boundary(u, 3, DIRICHLET, 0)
+@boundary(du, 3, DIRICHLET, 0)
+@boundary(v, 3, DIRICHLET, 0)
+@boundary(dv, 3, DIRICHLET, 0)
+@boundary(u, 4, DIRICHLET, 1)
+@boundary(du, 4, DIRICHLET, 0)
+@boundary(v, 4, DIRICHLET, 0)
+@boundary(dv, 4, DIRICHLET, 0)
+
+
+# Write the weak form
+@coefficient(f, "x^(-4)")
+
+
+@weakForm([du, dv, dp] ["", "Dt(u*v) + v*d(u, grad(u)) - grad(v)*p", "Cont(u,v)"])
+
+solve([du, dv, dp], [u, v, p], nonlinear=true);
+
+#analytical
+x = collect(1:1/num_elem/order:2);
+u_a = zeros(length(x));
+for i = 1:length(x)
+    u_a[i] = x[i]*(1+1.0/3*x[i]^2)^(-1/2);
+end
+
+#check integral error
+err_l2 = 0.0;
+for i = 1:length(x)-1
+    err_a = ((u.values[i]-u_a[i])^2+(u.values[i+1]-u_a[i+1])^2)/2.0*(1.0/num_elem/order);
+    global err_l2 += err_a;
+end
+
+print("L2 error = ", err_l2, "\n")
+# solution is stored in the variable's "values"
+#using Plots
+#pyplot();
+#display(plot(Femshop.grid_data.allnodes[:,1], Femshop.grid_data.allnodes[:,2], u.values, st = :surface))
+
+# check
+log_dump_config(Femshop.config);
+log_dump_prob(Femshop.prob);
+
+@finalize()
