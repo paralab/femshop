1dc8901fee99786f60ce78be7d2d438abd13d7c1
Eric Heisler <8408507+ericheisler@users.noreply.github.com>
2020-10-21 22:07:28 -0600

    fix boundary bug

--- interface/Julia/cg_boundary.jl
+++ interface/Julia/cg_boundary.jl
@@ -167,42 +167,43 @@ end
 function dirichlet_bc_rhs_only(b, val, bdryind, t=0, dofind = 1, totaldofs = 1)
     N = length(b);
     bdry = copy(bdryind);
+    vecbdry = copy(bdryind);
     if totaldofs > 1
-        bdry = (bdry .- 1) .* totaldofs .+ dofind;
+        vecbdry = (vecbdry .- 1) .* totaldofs .+ dofind;
     end
     
     if typeof(val) <: Number
         for i=1:length(bdry)
-            b[bdry[i]]=val;
+            b[vecbdry[i]]=val;
         end
         
     elseif typeof(val) == Coefficient && typeof(val.value[1]) == GenFunction
         if config.dimension == 1
             for i=1:length(bdry)
-                b[bdry[i]]=val.value[1].func(grid_data.allnodes[bdry[i],1],0,0,t);
+                b[vecbdry[i]]=val.value[1].func(grid_data.allnodes[bdry[i],1],0,0,t);
             end
         elseif config.dimension == 2
             for i=1:length(bdry)
-                b[bdry[i]]=val.value[1].func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],0,t);
+                b[vecbdry[i]]=val.value[1].func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],0,t);
             end
         else
             for i=1:length(bdry)
-                b[bdry[i]]=val.value[1].func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],grid_data.allnodes[bdry[i],3],t);
+                b[vecbdry[i]]=val.value[1].func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],grid_data.allnodes[bdry[i],3],t);
             end
         end
         
     elseif typeof(val) == GenFunction
         if config.dimension == 1
             for i=1:length(bdry)
-                b[bdry[i]]=val.func(grid_data.allnodes[bdry[i],1],0,0,t);
+                b[vecbdry[i]]=val.func(grid_data.allnodes[bdry[i],1],0,0,t);
             end
         elseif config.dimension == 2
             for i=1:length(bdry)
-                b[bdry[i]]=val.func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],0,t);
+                b[vecbdry[i]]=val.func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],0,t);
             end
         else
             for i=1:length(bdry)
-                b[bdry[i]]=val.func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],grid_data.allnodes[bdry[i],3],t);
+                b[vecbdry[i]]=val.func(grid_data.allnodes[bdry[i],1],grid_data.allnodes[bdry[i],2],grid_data.allnodes[bdry[i],3],t);
             end
         end
     end
